e1a259ba0da655f794811f565155f69d690c782b
accoc <accociesla@gmail.com>
2021-12-01 14:22:35 +0100

    tcp,udp,motors control for app

---
+++ App/modules.h
@@ -0,0 +1,15 @@
+#ifndef MODULES_H
+#define MODULES_H
+
+
+static const int      MODULE_NONE = 0x00;
+static const int    MODULE_STEVAL = 0x01;
+
+
+class Modules
+{
+public:
+    Modules();
+};
+
+#endif // MODULES_H
